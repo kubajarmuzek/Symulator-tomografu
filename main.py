090ae499b259119f704c418b636e861b40524cf9
kubajarmuzek <102328573+kubajarmuzek@users.noreply.github.com>
2024-05-12 18:59:30 +0200

    read and write to DICOM

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import tkinter as tk
 from skimage.transform import resize
 from skimage import img_as_ubyte
 from tkinter import ttk, messagebox,filedialog
+from pprint import pprint
 
 
 def radon_transform(image, scan_count, detector_count, angle_range, pad=True, plot=False):
@@ -213,6 +214,69 @@ def test_filtering(image, scans=360, detectors=360, angle=270):
     axs[2].imshow(y2, cmap='gray')
 
 
+def read_dicom(path):
+    from pydicom import dcmread
+    ds = dcmread(path)
+    # assume dicom metadata identifiers are uppercase
+    keys = {x for x in dir(ds) if x[0].isupper()} - {'PixelData'}
+    meta = {x: getattr(ds, x) for x in keys}
+    image = ds.pixel_array
+    return image, meta
+
+
+def write_dicom(path, image, meta):
+    from pydicom.dataset import Dataset, FileDataset, validate_file_meta
+    from pydicom.uid import generate_uid
+    from pydicom._storage_sopclass_uids import MRImageStorage
+
+    ds = Dataset()
+    ds.MediaStorageSOPClassUID = MRImageStorage
+    ds.MediaStorageSOPInstanceUID = generate_uid()
+    ds.TransferSyntaxUID = pydicom.uid.ExplicitVRLittleEndian
+
+    fd = FileDataset(path, {}, file_meta=ds, preamble=b'\0' * 128)
+    fd.is_little_endian = True
+    fd.is_implicit_VR = False
+
+    fd.SOPClassUID = MRImageStorage
+    fd.PatientName = 'Test^Firstname'
+    fd.PatientID = '123456'
+    now = datetime.datetime.now()
+    fd.StudyDate = now.strftime('%Y%m%d')
+
+    fd.Modality = 'MR'
+    fd.SeriesInstanceUID = generate_uid()
+    fd.StudyInstanceUID = generate_uid()
+    fd.FrameOfReferenceUID = generate_uid()
+
+    fd.BitsStored = 16
+    fd.BitsAllocated = 16
+    fd.SamplesPerPixel = 1
+    fd.HighBit = 15
+
+    fd.ImagesInAcquisition = '1'
+    fd.Rows = image.shape[0]
+    fd.Columns = image.shape[1]
+    fd.InstanceNumber = 1
+
+    fd.ImagePositionPatient = r'0\0\1'
+    fd.ImageOrientationPatient = r'1\0\0\0\-1\0'
+    fd.ImageType = r'ORIGINAL\PRIMARY\AXIAL'
+
+    fd.RescaleIntercept = '0'
+    fd.RescaleSlope = '1'
+    fd.PixelSpacing = r'1\1'
+    fd.PhotometricInterpretation = 'MONOCHROME2'
+    fd.PixelRepresentation = 1
+
+    for key, value in meta.items():
+        setattr(fd, key, value)
+
+    validate_file_meta(fd.file_meta, enforce_standard=True)
+
+    fd.PixelData = (image * 255).astype(np.uint16).tobytes()
+    fd.save_as(path)
+
 class TomographyApp(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -235,10 +299,19 @@ class TomographyApp(tk.Tk):
 
         self.load_button = ttk.Button(self.controls_frame, text="Load Image", command=self.load_image)
         self.load_button.pack(padx=10, pady=5)
+        self.load_dicom_button = ttk.Button(self.controls_frame, text="Load DICOM", command=self.load_dicom)
+        self.load_dicom_button.pack(padx=10, pady=5)
         self.transform_button = ttk.Button(self.controls_frame, text="Transform", command=self.perform_transform)
         self.transform_button.pack(padx=10, pady=5)
         self.reset_button = ttk.Button(self.controls_frame, text="Reset", command=self.reset_image)
         self.reset_button.pack(padx=10, pady=5)
+        self.save_button = ttk.Button(self.controls_frame, text="Save DICOM", command=self.save_dicom)
+        self.save_button.pack(padx=10, pady=5)
+
+        self.filtering_var = tk.BooleanVar()
+        self.filtering_var.set(True)  # Default value is True
+        self.filtering_checkbox = ttk.Checkbutton(self.controls_frame, text="Enable Filtering", variable=self.filtering_var)
+        self.filtering_checkbox.pack(padx=10, pady=5)
 
         self.output_image_frame = ttk.LabelFrame(self.main_frame, text="Output Image")
         self.output_image_frame.grid(row=0, column=2, padx=10, pady=10)
@@ -253,6 +326,108 @@ class TomographyApp(tk.Tk):
         self.sinogram_image = None
         self.sinogram_image_label = ttk.Label(self.sinogram_frame)
         self.sinogram_image_label.pack()
+
+        self.patient_info_frame = ttk.LabelFrame(self.main_frame, text="Patient Information")
+        self.patient_info_frame.grid(row=2, column=0, columnspan=3, padx=10, pady=10)
+
+        self.patient_name_label = ttk.Label(self.patient_info_frame, text="Patient Name:")
+        self.patient_name_label.grid(row=0, column=0, padx=5, pady=5)
+        self.patient_name_entry = ttk.Entry(self.patient_info_frame)
+        self.patient_name_entry.grid(row=0, column=1, padx=5, pady=5)
+
+        self.patient_id_label = ttk.Label(self.patient_info_frame, text="Patient ID:")
+        self.patient_id_label.grid(row=1, column=0, padx=5, pady=5)
+        self.patient_id_entry = ttk.Entry(self.patient_info_frame)
+        self.patient_id_entry.grid(row=1, column=1, padx=5, pady=5)
+
+        self.image_comments_label = ttk.Label(self.patient_info_frame, text="Image Comments:")
+        self.image_comments_label.grid(row=2, column=0, padx=5, pady=5)
+        self.image_comments_entry = ttk.Entry(self.patient_info_frame)
+        self.image_comments_entry.grid(row=2, column=1, padx=5, pady=5)
+
+        self.study_date_label = ttk.Label(self.patient_info_frame, text="Study Date:")
+        self.study_date_label.grid(row=3, column=0, padx=5, pady=5)
+        self.study_date_entry = ttk.Entry(self.patient_info_frame)
+        self.study_date_entry.grid(row=3, column=1, padx=5, pady=5)
+
+        self.dicom_info_frame = ttk.LabelFrame(self.main_frame, text="DICOM Image and Info")
+        self.dicom_info_frame.grid(row=3, column=0, columnspan=3, padx=10, pady=10)
+
+        self.dicom_image_label = ttk.Label(self.dicom_info_frame)
+        self.dicom_image_label.grid(row=0, column=0, padx=5, pady=5)
+
+        self.dicom_info_text = tk.Text(self.dicom_info_frame, height=10, width=50)
+        self.dicom_info_text.grid(row=0, column=1, padx=5, pady=5)
+
+
+    def load_dicom(self):
+        file_path = filedialog.askopenfilename(title="Select DICOM Image", filetypes=[("DICOM files", "*.dcm")])
+        if file_path:
+            try:
+                image, meta = read_dicom(file_path)
+
+                self.dicom_info_text.delete(1.0, tk.END)
+                for key, value in meta.items():
+                    self.dicom_info_text.insert(tk.END, f"{key}: {value}\n")
+
+                image = Image.fromarray(image)
+                image = ImageTk.PhotoImage(image)
+                self.dicom_image_label.config(image=image)
+                self.dicom_image_label.image = image
+
+                if meta:
+                    self.patient_name_entry.delete(0, tk.END)
+                    self.patient_name_entry.insert(0, meta.get('PatientName', ''))
+
+                    self.patient_id_entry.delete(0, tk.END)
+                    self.patient_id_entry.insert(0, meta.get('PatientID', ''))
+
+                    self.image_comments_entry.delete(0, tk.END)
+                    self.image_comments_entry.insert(0, meta.get('ImageComments', ''))
+
+                    self.study_date_entry.delete(0, tk.END)
+                    self.study_date_entry.insert(0, meta.get('StudyDate', ''))
+            except Exception as e:
+                messagebox.showerror("Error", str(e))
+
+
+    def save_dicom(self):
+        if self.default_image is None:
+            messagebox.showerror("Error", "Please load an image first.")
+            return
+
+        if self.output_image is None:
+            messagebox.showerror("Error", "No processed image available.")
+            return
+
+        try:
+            output_image_rescaled = rescale(self.output_image)
+            padded_output_image = circle_pad(output_image_rescaled)
+
+            # Prompt the user to choose the destination directory
+            save_path = filedialog.asksaveasfilename(
+                title="Save DICOM File",
+                filetypes=[("DICOM files", "*.dcm")],
+                defaultextension=".dcm"
+            )
+
+            if save_path:
+                patient_name = self.patient_name_entry.get()
+                patient_id = self.patient_id_entry.get()
+                image_comments = self.image_comments_entry.get()
+                study_date = self.study_date_entry.get()
+
+                meta = {
+                    'PatientName': patient_name,
+                    'PatientID': patient_id,
+                    'ImageComments': image_comments,
+                    'StudyDate': study_date,
+                }
+
+                write_dicom(save_path, padded_output_image, meta)
+                messagebox.showinfo("Success", "DICOM file saved successfully.")
+        except Exception as e:
+            messagebox.showerror("Error", str(e))
 
     def load_image(self):
         file_path = filedialog.askopenfilename(title="Select Image", filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])
@@ -289,10 +464,15 @@ class TomographyApp(tk.Tk):
             messagebox.showerror("Error", "Please load an image first.")
             return
 
+        filtering = self.filtering_var.get()
+
         try:
             sinogram = radon_transform(self.default_image, 360, 360, 270, plot=False)
-            sinogram_filtered = filter_sinogram(sinogram)
-            sinogram_rescaled = (sinogram_filtered * (255 / np.max(sinogram_filtered))).astype(np.uint8)  # Rescale to 0-255
+            if filtering:
+                sinogram_filtered = filter_sinogram(sinogram)
+                sinogram_rescaled = rescale(sinogram_filtered, min=0, max=255).astype(np.uint8)
+            else:
+                sinogram_rescaled = rescale(sinogram, min=0, max=255).astype(np.uint8)
             sinogram_image = ImageTk.PhotoImage(image=Image.fromarray(sinogram_rescaled))
             self.sinogram_image_label.config(image=sinogram_image)
             self.sinogram_image_label.image = sinogram_image
@@ -301,14 +481,20 @@ class TomographyApp(tk.Tk):
             return
 
         try:
-            output_image = inverse_radon(self.default_image.shape, sinogram_filtered, 270, filtering=True)
-            padded_output_image = circle_pad(output_image)
+            if filtering:
+                output_image = inverse_radon(self.default_image.shape, sinogram_filtered, 270, filtering=True)
+            else:
+                output_image = inverse_radon(self.default_image.shape, sinogram, 270, filtering=False)
+            output_image_rescaled = rescale(output_image, min=0, max=255).astype(np.uint8)
+            padded_output_image = circle_pad(output_image_rescaled)
             output_image_tk = ImageTk.PhotoImage(image=Image.fromarray(padded_output_image))
+            self.output_image = output_image
             self.output_image_label.config(image=output_image_tk)
             self.output_image_label.image = output_image_tk
         except Exception as e:
             messagebox.showerror("Error inverse", str(e))
             return
+
 
     def reset_image(self):
         self.default_image = None
@@ -322,7 +508,6 @@ class TomographyApp(tk.Tk):
 def main():
     app = TomographyApp()
     app.mainloop()
-
 
 if __name__ == "__main__":
     main()
